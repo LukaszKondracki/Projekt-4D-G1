5b1a36a6567d14ad41b879001a55bf25118c73d6
LK <technikum@kondracki.info>
2022-09-13 13:16:29 +0200

    contact

--- theme-switch.js
+++ theme-switch.js
@@ -2,41 +2,22 @@ const button = document.getElementById('theme-swap');
 
 const elements = document.getElementsByTagName('html');
 const html = elements[0];
+const store = window.localStorage;
 
-changeTheme();
+html.className = store.getItem('theme') || 'dark';
 
 button.addEventListener('click', () => {
 
-    
-    changeTheme();
-});
-
-function changeTheme() {
-    const theme = getTheme();
-
-    let isThemeLight = theme === 'light';
+    let isThemeLight = html.className === 'light';
 
     if (isThemeLight) {
-        html.classList.remove('light');
+        html.className = 'dark';
         button.innerText = "🌞";
-        setTheme('dark');
+        store.setItem('theme', 'dark');
     } else {
-        html.classList.add('light');
+        html.className = 'light';
         button.innerText = "🌚";
-        setTheme('light');
+        store.setItem('theme', 'light');
     }
-}
 
-function getTheme() {
-    const theme = localStorage.getItem('theme');
-    
-    if ('theme' === 'light') {
-        return 'light';
-    } 
-
-    return 'dark';
-}
-
-function setTheme(theme) {
-    localStorage.setItem('theme', theme);
-}
+});
