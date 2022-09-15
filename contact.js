695d9c0d403732510e410b5e180883cee252621a
Lukasz <technikum@kondracki.info>
2022-09-15 09:37:51 +0200

    form validation

--- contact.js
+++ contact.js
@@ -3,7 +3,29 @@ const form = document.getElementById('form');
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    // validation
+    const f = Object.values(form);
 
-    form.submit();
+    for (const input of f) {
+        console.log(`Input ${input.id} has value ${input.value}`);
+
+        switch (input.id) {
+            case 'email':
+                // walidacja emaila
+                break;
+            case 'name':
+                // walidacja imienia
+                break;
+            case 'body':
+                // walidacja tekstu
+                break;
+            case 'agree':
+                // walidacja zgody
+                break;
+            default: 
+                break;
+        }
+
+    }
+
+    // form.submit();
 });
