a05278d03819577c5913fafba77ff774b20cf578
Lukasz <technikum@kondracki.info>
2022-10-27 09:40:28 +0200

    CMS 2

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 13 Paź 2022, 08:58
+-- Czas generowania: 27 Paź 2022, 09:39
 -- Wersja serwera: 10.4.19-MariaDB
 -- Wersja PHP: 7.4.20
 
@@ -36,6 +36,13 @@ CREATE TABLE `blogposts` (
   `AuthorId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
+--
+-- Zrzut danych tabeli `blogposts`
+--
+
+INSERT INTO `blogposts` (`Id`, `Title`, `Body`, `CreatedAt`, `EditedAt`, `AuthorId`) VALUES
+(1, 'Lorem ipsum', 'skfb iwfhsk dfsdk ghsdkjgh sdkfj gsf asskfb iwfhsk dfsdk ghsdkjgh sdkfj gsf asskfb iwfhsk dfsdk ghsdkjgh sdkfj gsf asskfb iwfhsk dfsdk ghsdkjgh sdkfj gsf asskfb iwfhsk dfsdk ghsdkjgh sdkfj gsf asskfb iwfhsk dfsdk ghsdkjgh sdkfj gsf asskfb iwfhsk dfsdk ghsdkjgh sdkfj gsf asskfb iwfhsk dfsdk ghsdkjgh sdkfj gsf asskfb iwfhsk dfsdk ghsdkjgh sdkfj gsf asskfb iwfhsk dfsdk ghsdkjgh sdkfj gsf asskfb iwfhsk dfsdk ghsdkjgh sdkfj gsf asskfb iwfhsk dfsdk ghsdkjgh sdkfj gsf asskfb iwfhsk dfsdk ghsdkjgh sdkfj gsf asskfb iwfhsk dfsdk ghsdkjgh sdkfj gsf asskfb iwfhsk dfsdk ghsdkjgh sdkfj gsf asskfb iwfhsk dfsdk ghsdkjgh sdkfj gsf asskfb iwfhsk dfsdk ghsdkjgh sdkfj gsf asskfb iwfhsk dfsdk ghsdkjgh sdkfj gsf asskfb iwfhsk dfsdk ghsdkjgh sdkfj gsf asskfb iwfhsk dfsdk ghsdkjgh sdkfj gsf asskfb iwfhsk dfsdk ghsdkjgh sdkfj gsf asskfb iwfhsk dfsdk ghsdkjgh sdkfj gsf asskfb iwfhsk dfsdk ghsdkjgh sdkfj gsf as', '2022-10-27 09:14:19', NULL, 2);
+
 -- --------------------------------------------------------
 
 --
@@ -53,6 +60,26 @@ CREATE TABLE `messages` (
 -- --------------------------------------------------------
 
 --
+-- Struktura tabeli dla tabeli `roles`
+--
+
+CREATE TABLE `roles` (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `Color` varchar(7) COLLATE utf8mb4_bin NOT NULL,
+  `CanManage` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Zrzut danych tabeli `roles`
+--
+
+INSERT INTO `roles` (`Id`, `Name`, `Color`, `CanManage`) VALUES
+(1, 'Admin', '#ff0000', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Struktura tabeli dla tabeli `users`
 --
 
@@ -60,15 +87,17 @@ CREATE TABLE `users` (
   `Id` int(11) NOT NULL,
   `Name` varchar(30) COLLATE utf8mb4_bin NOT NULL,
   `Email` varchar(255) COLLATE utf8mb4_bin NOT NULL,
-  `Password` varchar(255) COLLATE utf8mb4_bin NOT NULL
+  `Password` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `RoleId` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
 -- Zrzut danych tabeli `users`
 --
 
-INSERT INTO `users` (`Id`, `Name`, `Email`, `Password`) VALUES
-(1, 'Julia', 'julia@julia.com', '$argon2id$v=19$m=65536,t=4,p=1$UWhGUms2WmQ5cEc1LlRDUA$/OhNpWp65eQrTOQTe04KXMgsg6BxW2RRaRSfpfxrFDU');
+INSERT INTO `users` (`Id`, `Name`, `Email`, `Password`, `RoleId`) VALUES
+(1, 'Julia', 'julia@julia.com', '$argon2id$v=19$m=65536,t=4,p=1$UWhGUms2WmQ5cEc1LlRDUA$/OhNpWp65eQrTOQTe04KXMgsg6BxW2RRaRSfpfxrFDU', NULL),
+(2, 'Admin', 'admin@admin.admin', '$argon2id$v=19$m=65536,t=4,p=1$ejc2dkw5eW9Bcy82VUV5Sw$Y6P6aiqQIpjglVSRZBFbjmZONFAPv7MQeL6Etatz4rs', 1);
 
 --
 -- Indeksy dla zrzutów tabel
@@ -78,7 +107,8 @@ INSERT INTO `users` (`Id`, `Name`, `Email`, `Password`) VALUES
 -- Indeksy dla tabeli `blogposts`
 --
 ALTER TABLE `blogposts`
-  ADD PRIMARY KEY (`Id`);
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `AuthorId` (`AuthorId`);
 
 --
 -- Indeksy dla tabeli `messages`
@@ -87,12 +117,20 @@ ALTER TABLE `messages`
   ADD PRIMARY KEY (`Id`);
 
 --
+-- Indeksy dla tabeli `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `Name` (`Name`);
+
+--
 -- Indeksy dla tabeli `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`Id`),
   ADD UNIQUE KEY `Name` (`Name`),
-  ADD UNIQUE KEY `Email` (`Email`);
+  ADD UNIQUE KEY `Email` (`Email`),
+  ADD KEY `fk_role_id` (`RoleId`);
 
 --
 -- AUTO_INCREMENT dla zrzuconych tabel
@@ -102,7 +140,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT dla tabeli `blogposts`
 --
 ALTER TABLE `blogposts`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT dla tabeli `messages`
@@ -111,10 +149,32 @@ ALTER TABLE `messages`
   MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
+-- AUTO_INCREMENT dla tabeli `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `blogposts`
+--
+ALTER TABLE `blogposts`
+  ADD CONSTRAINT `blogposts_ibfk_1` FOREIGN KEY (`AuthorId`) REFERENCES `users` (`Id`) ON DELETE CASCADE;
+
+--
+-- Ograniczenia dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`RoleId`) REFERENCES `roles` (`Id`) ON DELETE SET NULL;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
